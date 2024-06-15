d6270b431bebf1804532e14a23b9ba1e5763e5c7
Illia Chaban <illia.chaban.8@gmail.com>
2024-06-15 17:49:53 -0400

    udpate not

--- src/fn-utils/not.ts
+++ src/fn-utils/not.ts
@@ -1,4 +1,12 @@
-export const not =
-  <P extends any[]>(fn: (...args: P) => boolean) =>
-  (...args: P): boolean =>
-    !fn(...args);
+import { isFunction } from "../guards";
+import { Primitive } from "../types";
+
+export function not<P extends any[]>(
+  fn: (...args: P) => boolean
+): (...args: P) => boolean;
+export function not<T extends Primitive>(value: T): boolean;
+export function not(fnOrValue: ((...args: any[]) => boolean) | Primitive) {
+  if (isFunction(fnOrValue))
+    return (...args: any[]): boolean => !fnOrValue(...args);
+  return !fnOrValue;
+}
