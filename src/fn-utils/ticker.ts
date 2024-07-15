54eb9f4f23e4b98f0d69f1e962711061a8309a81
Illia Chaban <illia.chaban.8@gmail.com>
2024-07-15 11:41:50 -0400

    update ticker

--- src/fn-utils/ticker.ts
+++ src/fn-utils/ticker.ts
@@ -5,19 +5,50 @@
  *
  * window.addEventListener('scroll', ticker(runAnimation))
  */
-export const ticker = (callback: FrameRequestCallback) => {
-  let ticking = false;
+// export const ticker = (callback: FrameRequestCallback) => {
+//   let ticking = false;
 
-  const update: FrameRequestCallback = (...args) => {
-    callback(...args);
-    ticking = false;
+//   const update: FrameRequestCallback = (...args) => {
+//     callback(...args);
+//     ticking = false;
+//   };
+
+//   const requestTick = () => {
+//     if (ticking) return;
+//     requestAnimationFrame(update);
+//     ticking = true;
+//   };
+
+//   return requestTick;
+// };
+
+/**
+ * Handles events through requestAnimationFrame. Calls update function with latest provided arguments
+ *
+ * Use for animation (on scroll)
+ * @example
+ *
+ * window.addEventListener('scroll', ticker(runAnimation))
+ */
+export const ticker = <Args extends any[] = []>(
+  callback: (this: { time: DOMHighResTimeStamp }, ...args: Args) => void
+) => {
+  let animationFrameId: number | null = null;
+  let lastArgs: Args;
+
+  const requestTick = (...args: Args) => {
+    lastArgs = args;
+    if (animationFrameId !== null) return;
+    animationFrameId = requestAnimationFrame((time) => {
+      callback.call({ time }, ...lastArgs);
+      animationFrameId = null;
+    });
   };
 
-  const requestTick = () => {
-    if (ticking) return;
-    requestAnimationFrame(update);
-    ticking = true;
-  };
-
-  return requestTick;
+  return Object.assign(requestTick, {
+    cancel: () =>
+      animationFrameId !== null && cancelAnimationFrame(animationFrameId),
+  });
 };
+
+export { ticker as throttleRaf };
