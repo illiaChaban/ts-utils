1c804b1cda31e941673f9d5704e514319d9f049e
Illia Chaban <illia.chaban.8@gmail.com>
2024-11-11 13:10:17 -0500

    update assertValue

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -2,7 +2,7 @@ import { Truthy } from "../types";
 
 export function assert(
   value: unknown,
-  error: Error | string = "Assertion failed"
+  error: Error | string = "Assertion failed",
 ): asserts value {
   if (!value) {
     const err = typeof error === "string" ? new Error(error) : error;
@@ -10,13 +10,12 @@ export function assert(
   }
 }
 
-export const assertValue = <T>(
-  value: T,
-  error: Error | string = "Value assertion failed"
-): Truthy<T> => {
-  assert(value, error);
-  return value as any;
-};
+export const assertValue =
+  (error: Error | string = "Value assertion failed") =>
+  <T>(value: T): Truthy<T> => {
+    assert(value, error);
+    return value as any;
+  };
 
 export const panic = (msg = "Panic: unknown error") => {
   throw new Error(msg);
